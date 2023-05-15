98192654306b7889dc981510f219edf8909423de
chunkingz <fortuneking33@yahoo.com>
2023-05-15 14:12:16 -0500

    add node exercises 🎉

---
+++ node-exercises/README.md
@@ -0,0 +1,19 @@
+# CS472 (WAP)
+
+
+## Intro :sunglasses:
+
+This lab practices introduces the student to NodeJS.
+
+
+## Exercises :star:
+
+- Hello World
+- My Module
+- Request URL
+- Simple Adder
+
+
+## Hosted on Github Pages :octocat:
+
+:link: [Link to github pages](https://chunkingz.github.io/CS472/node-exercises)
---
+++ node-exercises/exercise-n4/AdderWebServer.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const addmod = require('./addmod.js');
+const PORT = 8080;
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    if (q.pathname=="/add.js") {
+        addmod.add(req,res,q.query)
+    } else {
+        fs.readFile(filename, function(err, data) {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                return res.end("404 Not Found");
+            } else {
+                res.writeHead(200);
+                res.write(data);
+                return res.end();
+            }
+        });
+    }
+}).listen(PORT, ()=>{
+        console.log(`Server running on port: ${PORT}`);
+});
